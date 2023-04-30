9a3094b1e47948e020a28a3c112c3084ed36d1a2
Zunairhussain <zunairhussain065@gmail.com>
2023-04-30 19:11:36 +0500

    java

--- style .js
+++ style .js
@@ -1,33 +1,58 @@
-// var students = [];
+var students = [];
 
-// function addUser() {
-//     students[students.length] = document.getElementById("userName").value
-//     printStudents()
-// }
-
-
-// function findUser() {
-//     var searchValue = document.getElementById("searchInput").value;
-//     var result = "not found";
-//     for (let index = 0; index < students.length; index++) {
-//         const student = students[index];
-//         if (student == searchValue) {
-//             result = `user found on index ${index}`
-//             break;
-//         }
-//     }
-//     document.getElementById("searchResult").innerHTML = result
-// }
-
-// function printStudents() {
-//     console.log('====================================');
-//     console.log(students);
-//     console.log('====================================');
-// }
+function addUser() {
+    students.push( document.getElementById("userName").value)
+    printStudents()
+}
 
 
-// var studentsNames =["Umair","Ahmad","Abdullah"]
-// studentsNames.push=["Naveed"]
+function findUser() {
+    var searchValue = document.getElementById("searchInput").value;
+    var result = "not found";
+    for (let index = 0; index < students.length; index++) {
+        const student = students[index];
+        if (student == searchValue) {
+            result = `user found on index ${index}`
+            break;
+        }
+    }
+    document.getElementById("searchResult").innerHTML = result
+}
+
+function printStudents() {
+    console.log('====================================');
+    console.log(students);
+    console.log('====================================');
+}
+
+
+var studentsNames =["Umair","Ahmad","Abdullah"]
+studentsNames.push=["Naveed"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var number =[10,20,30,40]
 
@@ -38,9 +63,7 @@
 // console.log('=========================================');
 
 
-
-
-
+// User grade 
 
 // var grade = "A"
 
@@ -71,56 +94,66 @@
 //         break;
 // }
 
-var day = "tuesday"
+// var day = "Tuesday"
 
-console.log(performance.now());
+// console.log(performance.now());
 
-switch (day) {
-    case "Sunday":
-        console.log('=========================================');
-        console.log("Grade is A"); 
-        console.log('=========================================');
+// switch (day) {
+//     case "Sunday":
+//         console.log('=========================================');
+//         console.log("Grade is A"); 
+//         console.log('=========================================');
 
-        break;
-    case "Monday" :
-        console.log('=========================================');
-    console.log("Grade is B"); 
-    console.log('=========================================');
+//         break;
+//     case "Monday" :
+//         console.log('=========================================');
+//     console.log("Grade is B"); 
+//     console.log('=========================================');
 
-        break;
-    case "Tuesday":
-        console.log('=========================================');
-        console.log("Grade is C"); 
-        console.log('=========================================');
-
-
-    default:
-        console.log('=========================================');
-        console.log("Grade is default"); 
-        console.log('=========================================');
-
-        console.log(performance.now());
+//         break;
+//     case "Tuesday":
+//         console.log('=========================================');
+//         console.log("Grade is C"); 
+//         console.log('=========================================');
 
 
+//     default:
+//         console.log('=========================================');
+//         console.log("Grade is default"); 
+//         console.log('=========================================');
 
-        break;
+//         console.log(performance.now());
 
-
-
-
-    }
-
-
-
+//         break;
 
 
+//     }
 
 
+// loop process 
+
+// for (let i = 1; i < 10.;i++){
+//     console.log("for loop")
+// }
 
 
+// var index = 1;
+// while (index > 10) {
+//     console.log("while loop", index )
+// index++
+// }
+
+// var anyVar =1;
+
+// do {
+//     console.log("do while",anyVar);
+// // if (anyVar == 7) {
+// //     break ;
+
+//   anyVar++; 
+// }while (anyVar < 10)
 
 
-
-
+var userNumber = ["a","b","c","d","e"]
 
 
