30d8c78be88ca9ef5074fe3e1e7271e6b50c3aeb
Zunairhussain <zunairhussain065@gmail.com>
2023-04-30 10:54:45 +0500

    java script

--- style .js
+++ style .js
@@ -1,26 +1,30 @@
-var students = [];
+// var students = [];
 
-function addUser() {
-    students[students.length] = document.getElementById("userName").value
-    printStudents()
-}
+// function addUser() {
+//     students[students.length] = document.getElementById("userName").value
+//     printStudents()
+// }
 
 
-function findUser() {
-    var searchValue = document.getElementById("searchInput").value;
-    var result = "not found";
-    for (let index = 0; index < students.length; index++) {
-        const student = students[index];
-        if (student == searchValue) {
-            result = `user found on index ${index}`
-            break;
-        }
-    }
-    document.getElementById("searchResult").innerHTML = result
-}
+// function findUser() {
+//     var searchValue = document.getElementById("searchInput").value;
+//     var result = "not found";
+//     for (let index = 0; index < students.length; index++) {
+//         const student = students[index];
+//         if (student == searchValue) {
+//             result = `user found on index ${index}`
+//             break;
+//         }
+//     }
+//     document.getElementById("searchResult").innerHTML = result
+// }
 
-function printStudents() {
-    console.log('====================================');
-    console.log(students);
-    console.log('====================================');
-}
+// function printStudents() {
+//     console.log('====================================');
+//     console.log(students);
+//     console.log('====================================');
+// }
+
+
+var studentsNames =["Umair","Ahmad","Abdullah"]
+studentsNames.push=["Naveed"]
